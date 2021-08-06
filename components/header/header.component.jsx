c9b14f8fc2704e9118c7f815996c5908e192a781
anthonysea <anthonyqchao@gmail.com>
2021-08-06 13:03:40 -0700

    updated readme.md; changed header title to link back to index

--- components/header/header.component.jsx
+++ components/header/header.component.jsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React from "react";
+
+import Link from "next/link";
 
 const Header = ({}) => {
-	return (
-		<header className="text-3xl font-bold">
-			<h1>MENU Skateshop Raffles</h1>
-			{/* Add an if authenticated -> render 'sign out' and 'new raffle' buttons */}
-		</header>
-	)
+  return (
+    <header className="text-3xl font-bold">
+      <h1>
+        <Link href="/">
+          <a>MENU Skateshop Raffles</a>
+        </Link>
+      </h1>
+      {/* Add an if authenticated -> render 'sign out' and 'new raffle' buttons */}
+    </header>
+  );
 };
 
 export default Header;
